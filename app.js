6e74afbccdd981824bc00884fb0ee8099ae00a7e
David Rospond <drospond@sentiment360.com>
2020-08-26 10:16:27 -0400

    prompt displays directories

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-const getParams = {
-  Bucket: "atlscribes.org-recordings",
-  Key: "2020-06-15 Full Council/VoiceMessage (1).wav",
-};
 
 // const storage = new Storage({
 //     keyFilename: <server-key-file-path>,
 //  });
+
+const getParams = {
+  Bucket: "atlscribes.org-recordings",
+  Key: "2020-06-15 Full Council/VoiceMessage (1).wav",
+};
 
 // s3.getObject(getParams, function (err, data) {
 //   if (err) {
@@ -24,6 +25,19 @@ const getParams = {
 //     console.log(data.Body);
 //   }
 // });
+
+function testPrompt(){
+    inquirer.prompt([
+        {
+          type: "checkbox",
+          name: "answer",
+          message: "Select a directory of public comments to import to firebase.",
+          choices: directories
+        }
+      ]).then((res)=>{
+          console.log(`Your answer: ${res.answer}`);
+    })
+  }
 
 const listObjectParams = {
   Bucket: "atlscribes.org-recordings",
@@ -39,21 +53,10 @@ s3.listObjectsV2(listObjectParams, (err, data) => {
     data.CommonPrefixes.forEach(dir => {
         directories.push(dir.Prefix);
     })
-    // console.log(directories);
+    testPrompt();
   }
 });
 
-function testPrompt(){
-    inquirer.prompt([
-        {
-          type: "checkbox",
-          name: "answer",
-          message: "Select a directory of public comments to import to firebase.",
-          choices: directories
-        }
-      ]).then((res)=>{
-          console.log(`Your answer: ${res.answer}`);
-    })
-  }
 
-  testPrompt();
+
+  
