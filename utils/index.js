7d325f9cf9f325cbaeb77af167561abd20c68a1e
David Rospond <drospond@sentiment360.com>
2020-08-26 16:35:08 -0400

    util function to construct firebase object

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,27 @@
+
+/**
+ * 
+ * @param text firebase object key
+ */
 exports.urlEncodeS3Key = function(text){
     return `https://s3.us-east-1.amazonaws.com/atlscribes.org-recordings/${encodeURIComponent(text)}`
+}
+
+exports.constructEntryObject = function(textFile, wavFile){
+    const getParams = {
+        Bucket: "atlscribes.org-recordings",
+        Key: textFile.Key,
+    };
+    s3.getObject(getParams, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            entryObject ={
+                wavURl: utils.urlEncodeS3Key(wavFile.Key),
+                wavETag: wavFile.ETag,
+                text: data.Body.toString()
+            }
+            return entryObject;
+        }
+    });
 }
