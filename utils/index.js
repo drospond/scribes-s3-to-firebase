5e3ecde44c0f713b2f01a1240ea4ef609d5f1cee
David Rospond <drospond@sentiment360.com>
2020-08-26 23:12:03 -0400

    function to pair and parse wav and txt files

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,15 @@
+require("dotenv").config();
+const s3 = require("../databaseConfig/s3Config");
 
 /**
  * 
  * @param text firebase object key
  */
-exports.urlEncodeS3Key = function(text){
+const urlEncodeS3Key = function(text){
     return `https://s3.us-east-1.amazonaws.com/atlscribes.org-recordings/${encodeURIComponent(text)}`
 }
 
-exports.constructEntryObject = function(textFile, wavFile){
+const constructEntryObject = function(textFile, wavFile){
     const getParams = {
         Bucket: "atlscribes.org-recordings",
         Key: textFile.Key,
@@ -17,7 +19,7 @@ exports.constructEntryObject = function(textFile, wavFile){
             console.log(err);
         } else {
             entryObject ={
-                wavURl: utils.urlEncodeS3Key(wavFile.Key),
+                wavURl: urlEncodeS3Key(wavFile.Key),
                 wavETag: wavFile.ETag,
                 text: data.Body.toString()
             }
@@ -25,3 +27,26 @@ exports.constructEntryObject = function(textFile, wavFile){
         }
     });
 }
+
+const entryPairing = function (entries) {
+    for (let i = 0; i < entries.length; i++) {
+      let textFile = false;
+      let wavFile = false;
+      const fileExtension = entries[i].Key.split(".").pop();
+      const entryKey = entries[i].Key.substr(0, entries[i].Key.length - 4);
+      if (fileExtension === "txt") {
+        textFile = entries[i];
+        wavFile = entries.find((entry) => {
+          if (entry.Key === `${entryKey}.wav`) return true;
+        });
+        if (!wavFile) {
+          console.log("No match found for: ", entries[i].Key);
+        }
+      } else {
+        continue;
+      }
+      if (textFile && wavFile) {
+        constructEntryObject(textFile, wavFile);
+      }
+    }
+  };
